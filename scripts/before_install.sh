f9e34a44f1541e60da8f0c0fdf16c30ce8b55d3e
EC2 Default User <ec2-user@ip-172-31-35-122.us-west-1.compute.internal>
2026-01-16 13:25:36 +0000

    Adding a cleaning command before the install

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,3 +8,5 @@ echo "Unmounting EFS if mounted..."
 if mount | grep -q "/var/www/html/wp-content"; then
     umount -f /var/www/html/wp-content
 fi
+echo "Cleaning /var/www/html directory (including hidden files)..."
+rm -rf /var/www/html/* /var/www/html/.[!.]* /var/www/html/..?*
